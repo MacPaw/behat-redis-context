b0524c8f94751200af9d178adb4ac38e423f9160
Yozhef <hisemjo@gmail.com>
2022-01-12 15:29:47 +0200

    feat: add redmi behat config

--- tests/DependencyInjection/BehatRedisContextExtensionTest.php
+++ tests/DependencyInjection/BehatRedisContextExtensionTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace BehatRedisContext\Tests\DependencyInjection;
 
+use BehatRedisContext\Context\RedisContext;
+use BehatRedisContext\Context\RedisFixturesContext;
 use BehatRedisContext\DependencyInjection\BehatRedisContextExtension;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -20,7 +22,7 @@ class BehatRedisContextExtensionTest extends TestCase
 
         $extension->load([['dataFixturesPath' => 'src/DataFixture']], $container);
 
-        $this->assertTrue($container->has('behat_redis_context.redis_context'));
-        $this->assertTrue($container->has('behat_redis_context.redis_fixture_context'));
+        $this->assertTrue($container->has(RedisContext::class));
+        $this->assertTrue($container->has(RedisFixturesContext::class));
     }
 }
